e8320ecfc9e98e2d78c9944cd99277ed4377f512
Jens Piegsa <piegsa@gmail.com>
2021-08-13 18:02:51 +0200

    sounds

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 
+const minBreathCount = 20;
 const maxBreathCount = 40;
 const inhaleExhaleLengthRatio = 0.7;
-const fullHoldSeconds = 15;
-const breatheDurationInSeconds = 2.0;
+const lastPhaseFullHoldSeconds = 15;
+const breatheDurationInSeconds = 2.65;
 
 const roundLabel = document.getElementById("round");
 const phaseLabel = document.getElementById("phase");
@@ -11,12 +12,14 @@ const breathCounter = document.getElementById("counter");
 const clockLabel = document.getElementById("clock");
 
 let currentBreathCount = 1;
-let round = 1;
-let phase = 0;
+let currentRound = 1;
+let currentPhase = 0;
 
 let startTime, deltaTimeInSeconds;
 
-let click, minuteSounds;
+const totalSounds = 13;
+let fullBreathSound, inhaleSound, exhaleSound, xMinutesPassedSounds;
+let loadedSounds = 0;
 
 const breathCounterAnimation = breathCounter.animate([
     {opacity: 0.0, transform: "scale(0.0)", filter: "blur(2vh)"},
@@ -37,7 +40,7 @@ breathCounterAnimation.onfinish = function () {
     if (currentBreathCount <= maxBreathCount) {
         breathCounter.textContent = "" + currentBreathCount;
         breathCounterAnimation.play();
-        click.play();
+        playFullBreathSound();
     } else {
         startPhaseTwo();
     }
@@ -47,16 +50,21 @@ clockAnimation.onfinish = function () {
     deltaTimeInSeconds = Math.floor((Date.now() - startTime) / 1000);
     const minutes = Math.floor(deltaTimeInSeconds / 60);
     const seconds = deltaTimeInSeconds % 60;
-    clockLabel.textContent = pad(minutes, 2) + ":" + pad(seconds, 2);
-    if (phase === 2) {
+    if (currentPhase === 2) {
+        clockLabel.textContent = padInteger(minutes, 2) + ":" + padInteger(seconds, 2);
         clockAnimation.play();
         if (seconds === 0) {
             if (minutes <= 10) {
-                minuteSounds[minutes - 1].play();
+                playMinutesPassedSound(minutes);
             }
         }
-    } else if (phase === 3) {
-        if (deltaTimeInSeconds <= fullHoldSeconds) {
+    } else if (currentPhase === 3) {
+        if (deltaTimeInSeconds <= lastPhaseFullHoldSeconds) {
+            clockLabel.textContent = padInteger(minutes, 2) + ":" + padInteger(seconds, 2);
+            clockAnimation.play();
+        } else if (deltaTimeInSeconds === lastPhaseFullHoldSeconds + 1) {
+            playExhaleSound();
+            clockLabel.style.display = "none";
             clockAnimation.play();
         } else {
             nextRound();
@@ -65,20 +73,20 @@ clockAnimation.onfinish = function () {
 }
 
 function startPhaseOne() {
-    roundLabel.textContent = "round " + round;
-    phase = 1;
-    phaseLabel.textContent = "phase " + phase;
-    instructionLabel.textContent = "Breathe deeply 20-40 times and exhale after the last breath.";
+    playFullBreathSound();
+    roundLabel.textContent = "round " + currentRound;
+    currentPhase = 1;
+    phaseLabel.textContent = "phase " + currentPhase;
+    instructionLabel.textContent = `Breathe deeply ${minBreathCount}-${maxBreathCount} times and exhale after the last breath.`;
     breathCounter.style.display = "inline-block";
     breathCounter.textContent = "1";
     breathCounterAnimation.play();
-    click.play();
 }
 
 function startPhaseTwo() {
-    phase++;
-    phaseLabel.textContent = "phase " + phase;
-    instructionLabel.textContent = "Hold your breath as long as it's comfortable and tap on inhale.";
+    currentPhase++;
+    phaseLabel.textContent = "phase " + currentPhase;
+    instructionLabel.textContent = "Hold your breath as long as it's comfortable for you and tap.";
 
     currentBreathCount = 1;
     breathCounter.style.display = "none";
@@ -92,9 +100,10 @@ function startPhaseTwo() {
 }
 
 function startPhaseThree()  {
-    phase++;
-    phaseLabel.textContent = "phase " + phase;
-    instructionLabel.textContent = "Inhale deeply and hold your breath for " + fullHoldSeconds + " seconds.";
+    playInhaleSound();
+    currentPhase++;
+    phaseLabel.textContent = "phase " + currentPhase;
+    instructionLabel.textContent = `Inhale deeply, hold your breath for ${lastPhaseFullHoldSeconds} seconds and exhale.`;
 
     clockLabel.classList.add("hold-empty");
     clockLabel.textContent = "00:00";
@@ -108,41 +117,108 @@ function nextRound() {
     clockAnimation.pause();
     clockAnimation.currentTime = 0;
 
-    round++;
+    currentRound++;
     startPhaseOne();
 }
 
 function loadSounds() {
-    click = new Audio("sounds/click.mp3");
-    minuteSounds = [
-        new Audio("sounds/1min.mp3"),
-        new Audio("sounds/2min.mp3"),
-        new Audio("sounds/3min.mp3"),
-        new Audio("sounds/4min.mp3"),
-        new Audio("sounds/5min.mp3"),
-        new Audio("sounds/6min.mp3"),
-        new Audio("sounds/7min.mp3"),
-        new Audio("sounds/8min.mp3"),
-        new Audio("sounds/9min.mp3"),
-        new Audio("sounds/10min.mp3")];
+    fullBreathSound = loadSound("sounds/full-breath.mp3");
+    inhaleSound = loadSound("sounds/inhale.mp3");
+    exhaleSound = loadSound("sounds/exhale.mp3");
+
+    xMinutesPassedSounds = [];
+    for (let i = 1; i <= 10; i++) {
+        xMinutesPassedSounds.push(loadSound(`sounds/${i}min.mp3`));
+    }
 }
 
-function skipForwardToPhaseTwo() {
-    if (phase === 0) {
-        loadSounds();
+function loadSound(file) {
+    let audio = new Audio(file);
+    audio.load();
+    audio.addEventListener("canplaythrough", onSoundLoaded);
+    return audio;
+}
+
+
+function onSoundLoaded() {
+    loadedSounds++;
+    if (loadedSounds === totalSounds) {
+        phaseLabel.textContent = "tap to start";
+        document.body.addEventListener("click", onClick, true);
+    }
+}
+
+
+function onClick() {
+    if (currentPhase === 0) {
         startPhaseOne();
-    } else if (phase === 1) {
+    } else if (currentPhase === 1) {
         startPhaseTwo();
-    } else if (phase === 2) {
+    } else if (currentPhase === 2) {
         startPhaseThree();
     }
 }
 
-function pad(num, size) {
+function playFullBreathSound() {
+    fullBreathSound.currentTime = 0;
+    fullBreathSound.play();
+}
+
+function playInhaleSound() {
+    inhaleSound.currentTime = 0;
+    inhaleSound.play();
+}
+
+function playExhaleSound() {
+    exhaleSound.currentTime = 0;
+    exhaleSound.play();
+}
+
+function playMinutesPassedSound(minutes) {
+    let sound = xMinutesPassedSounds[minutes - 1];
+    sound.currentTime = 0;
+    sound.play();
+}
+
+function padInteger(num, size) {
     num = num.toString();
     while (num.length < size) num = "0" + num;
     return num;
 }
 
-document.body.addEventListener("click", skipForwardToPhaseTwo, true);
+const pullStart = {x: 0, y: 0};
+const pullEnd = {x: 0, y: 0};
 
+function onTouchStart(e) {
+    if (typeof e["targetTouches"] !== "undefined") {
+        pullStart.x = e.targetTouches[0].screenX;
+        pullStart.y = e.targetTouches[0].screenY;
+    } else {
+        pullStart.x = e.screenX;
+        pullStart.y = e.screenY;
+    }
+}
+
+function onTouchEnd(e) {
+    if (typeof e["changedTouches"] !== "undefined") {
+        pullEnd.x = e.changedTouches[0].screenX;
+        pullEnd.y = e.changedTouches[0].screenY;
+    } else {
+        pullEnd.x = e.screenX;
+        pullEnd.y = e.screenY;
+    }
+
+    const dY = pullStart.y - pullEnd.y;
+    const dX = pullStart.x - pullEnd.x;
+    let isPullDown = dY < 0
+        && ((Math.abs(dX) <= 100 && Math.abs(dY) >= 300)
+            || (Math.abs(dX) / Math.abs(dY) <= 0.3 && dY >= 60));
+    if (isPullDown) {
+        location.reload();
+    }
+}
+
+document.addEventListener("touchstart", onTouchStart, false);
+document.addEventListener("touchend", onTouchEnd, false);
+
+loadSounds();
