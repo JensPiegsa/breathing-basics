9bd25296d431a914214b641b93544e450b318c00
Jens Piegsa <piegsa@gmail.com>
2021-09-05 22:32:30 +0200

    more colors

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ const fullBreathDurationInSeconds = 2.65;
 const inhaleExhaleLengthRatio = 0.7;
 const phaseThreeHoldSeconds = 15;
 
+const body = document.body;
 const roundLabel = document.getElementById("round");
 const phaseLabel = document.getElementById("phase");
 const instructionLabel = document.getElementById("instruction");
@@ -46,6 +47,15 @@ const breathAnimation = new Animation(new KeyframeEffect(breathCountLabel, [
     {opacity: 0.0, transform: "scale(0.0)"}
 ], {duration: fullBreathDurationInSeconds * 1000, fill: "backwards"}));
 
+/* animation used in first phase */
+const breathBackgroundAnimation = new Animation(new KeyframeEffect(body, [
+    {backgroundSize: "100vh"},
+    {backgroundSize: "700vh", offset: inhaleExhaleLengthRatio * 0.75},
+    {backgroundSize: "750vh", offset: inhaleExhaleLengthRatio},
+    {backgroundSize: "350vh", offset: inhaleExhaleLengthRatio * 1.2},
+    {backgroundSize: "100vh"}
+], {duration: fullBreathDurationInSeconds * 1000, fill: "backwards"}));
+
 /* animation used in second and third phase */
 const clockAnimation = new Animation(new KeyframeEffect(clockLabel, [
     {opacity: 0.8, transform: "translateY(0)"},
@@ -65,6 +75,14 @@ const clockHideAnimation = new Animation(new KeyframeEffect(clockLabel, [
     {opacity: 0.0, transform: "scale(0.0)"}
 ],  {duration: fullBreathDurationInSeconds * (1.0 - inhaleExhaleLengthRatio) * 1000, fill: "backwards"}));
 
+/* animation used in third phase */
+const holdBackgroundAnimation = new Animation(new KeyframeEffect(body, [
+    {backgroundSize: "100vh"},
+    {backgroundSize: "750vh", offset: 1.0 / (phaseThreeHoldSeconds + fullBreathDurationInSeconds + 1.0)},
+    {backgroundSize: "750vh", offset: 1.0 - (1.0 / (phaseThreeHoldSeconds + fullBreathDurationInSeconds + 1.0))},
+    {backgroundSize: "100vh"}
+], {duration: (phaseThreeHoldSeconds + fullBreathDurationInSeconds + 1) * 1000, fill: "backwards"}));
+
 /* animation used in the ending */
 const endingAnimation = new Animation(new KeyframeEffect(instructionLabel, [
     {opacity: 0.8, transform: "translateY(0)"},
@@ -81,6 +99,7 @@ breathAnimation.onfinish = function () {
         breathCountLabel.textContent = "" + currentBreathCount;
         playFullBreathSound();
         breathAnimation.play();
+        breathBackgroundAnimation.play();
     } else {
         startPhaseTwo();
     }
@@ -128,6 +147,7 @@ function onClickFinish() {
     currentPhase = -1;
 
     reset(breathAnimation);
+    reset(breathBackgroundAnimation);
     reset(clockHideAnimation);
     reset(clockAnimation);
 
@@ -173,6 +193,7 @@ function startPhaseOne() {
     breathCountLabel.style.display = "inline-block";
     breathCountLabel.textContent = "1";
     breathAnimation.play();
+    breathBackgroundAnimation.play();
 }
 
 function startPhaseTwo() {
@@ -185,6 +206,7 @@ function startPhaseTwo() {
     currentBreathCount = 1;
     breathCountLabel.style.display = "none";
     reset(breathAnimation);
+    reset(breathBackgroundAnimation);
 
     clockLabel.textContent = "00:00";
     clockLabel.style.display = "inline-block";
@@ -204,6 +226,7 @@ function startPhaseThree()  {
     clockLabel.textContent = "00:00";
 
     clockShowAnimation.play();
+    holdBackgroundAnimation.play();
 }
 
 function nextRound() {
